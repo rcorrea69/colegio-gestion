b27b462976f4b4bf1627af65dc86837b3206845c
rcorrea69 <rcorrea@derecho.uba.ar>
2021-11-11 19:01:59 -0300

    sigo solucionando gastos

--- ajax/registraGastos.php
+++ ajax/registraGastos.php
@@ -9,9 +9,9 @@ $fecha=$_POST['fecha'];
 $rubro=$_POST['rubro'];
 $descripcion=$_POST['descripcion'];
 $importe=$_POST['importe'];
-$tipogasto=$_POST['tipogasto'];
-$proveedor=$_POST['proveedor'];
 $subrubro=$_POST['subrubro'];
+$proveedor=0;
+$tipogasto=$_POST['tipogasto'];
 $usuario=$_SESSION['id_usuario'];
 
 
@@ -19,20 +19,19 @@ $usuario=$_SESSION['id_usuario'];
 
 // $repuesta= "este es el post enviado desde registraventa.php". $_POST['detalle'];
 
-
 ////////grabo la linea de gastos y capturo el id insertado///////////////////////////
-$sql="INSERT INTO `gastos` (`id_proveedor`, `ga_fecha`, `ga_descripcion`, `ga_importe`, `ga_rubro`, `ga_subrubro`, `ga_usuario`)
-VALUES ($proveedor,'".$fecha."','".$descripcion."',$importe,$rubro,$subrubro,$usuario)";
+$sql="INSERT INTO `gastos` (`tipo_gasto`, `id_proveedor`, `ga_fecha`, `ga_descripcion`, `ga_importe`, `ga_rubro`, `ga_subrubro`, `ga_usuario`)
+VALUES ($tipogasto,$proveedor,'".$fecha."','".$descripcion."',$importe,$rubro,$subrubro,$usuario)";
 $res = mysqli_query($link, $sql);
 $idvta=mysqli_insert_id($link);//obtengo el id de ventas
 
 //die($sql);
 /////////////////////////pregunto el gasto es  cta cte y la registtro en clientes_ctacte/////////////////////
 
-if($tipogasto==1){
-        $sqlctacte="INSERT INTO `proveedores_ctacte`(`id_proveedor`, `ctacteDH`, `factura_recibo`, `ctacte_importe`,`ctacte_fecha`) 
-        VALUES ($proveedor,'H',$idvta,$importe,'".$fecha."')";
-        $ejectutoctacte=mysqli_query($link, $sqlctacte);     
-        //die($sqlctacte);   
-}
+// if($tipogasto==1){
+//         $sqlctacte="INSERT INTO `proveedores_ctacte`(`id_proveedor`, `ctacteDH`, `factura_recibo`, `ctacte_importe`,`ctacte_fecha`) 
+//         VALUES ($proveedor,'H',$idvta,$importe,'".$fecha."')";
+//         $ejectutoctacte=mysqli_query($link, $sqlctacte);     
+//         //die($sqlctacte);   
+// }
 echo "este es el id de ventas generado".$idvta;
--- js/gastos.js
+++ js/gastos.js
@@ -4,60 +4,106 @@ $(document).ready(function () {
     $("#grabar").click(function (e) {
         e.preventDefault();
         var fecha = $("#fecha").val();
-        var rubro = parseInt($("#rubro").val());
         var descripcion = $("#descripcion").val();
         var importe = parseFloat($("#importe").val());
-        var tipogasto = parseInt($("#tipogasto").val());
-        var proveedor = parseInt($("#proveedor").val());
-        var subrubro = parseInt($("#subrubro").val());
+        var subrubro = 0;
+        var rubro = 0;
+        var tipogasto=0;
+
+        if ($("#Grubros").is(':checked')) {
+            subrubro = parseInt($("#subrubro").val());
+            rubro = parseInt($("#rubro").val());
+            tipogasto=1;
+
+        };
+        if (descripcion == null || descripcion.length == 0 || /^\s+$/.test(descripcion)) {
+            //alert('El dni esta vacio desde aca');
+            $("#descripcion").focus();
+            Swal.fire({
+              icon: "warning",
+              title: "Atención...",
+              text: "Debe ingresar una Descripción del Gasto",
+              //footer: '<a href>Why do I have this issue?</a>'
+            });
+            return false;
+        };
+
+        if (!$.isNumeric(importe)) {
+            $("#importe").focus();;
+            Swal.fire({
+              icon: "warning",
+              title: "Atención...",
+              text: "Debe ingresar un Importe Valido",
+            });
+            return false;
+          };
+        if(tipogasto===1 && rubro==0  ){
+
+            console.log('tipo de gasto :'+tipogasto);
+            console.log('rubro :'+rubro);
+            console.log('tipo Subrubro :'+subrubro);
+
+            Swal.fire({
+                icon: "warning",
+                title: "Atención...",
+                text: "El Gasto que intenta ingresar es por Rubros, Debe Seleccionar un Rubro",
+              });
+              return false;
+        };
+        if(tipogasto===1 && subrubro==0  ){
+
+            console.log('tipo de gasto :'+tipogasto);
+            console.log('rubro :'+rubro);
+            console.log('tipo Subrubro :'+subrubro);
+
+            Swal.fire({
+                icon: "warning",
+                title: "Atención...",
+                text: "El Gasto que intenta ingresar es por Rubros, Debe Seleccionar un Subrubro",
+              });
+              return false;
+        };
 
         console.log(fecha);
-        console.log('Rubro : '+rubro);
-        console.log('Descripcion : '+descripcion);
-        console.log('Importe : '+importe);
-        console.log('Tipo de gasto : '+tipogasto);
-        console.log('Proveedor : '+proveedor);
+        console.log('Rubro : ' + rubro);
+        console.log('subRubro : ' + subrubro);
+        console.log('Descripcion : ' + descripcion);
+        console.log('Importe : ' + importe);
 
-        if (proveedor == 0 && tipogasto == 1) {
-            Swal.fire({
-            icon: "error",
-            title: "Atención...",
-            text: "La Compras o Gasto cta-cte. Debe seleccionar un Proveedor Valido!",
-            });
-        } else {
-            alert('entree');
-            $.ajax({
-                type: "POST",
-                url: "./ajax/registraGastos.php",
-                data: {rubro: rubro,fecha: fecha,descripcion: descripcion,importe: importe,tipogasto:tipogasto,proveedor:proveedor,subrubro:subrubro},
-               
-                success: function (response) {
-                    alert("Genial ok ................."+response);
-                },
-            });
-        }
+
+
+        alert('entree');
+        $.ajax({
+            type: "POST",
+            url: "./ajax/registraGastos.php",
+            data: { rubro: rubro, fecha: fecha, descripcion: descripcion, importe: importe, subrubro: subrubro,tipogasto:tipogasto },
+            success: function (response) {
+                alert("Genial ok ................." + response);
+            },
+        });
+
     });
 
 
-    function cboRubros(){
+    function cboRubros() {
         $.ajax({
             type: "POST",
             url: "./ajax/cboRubros.php",
             success: function (response) {
                 $('#rubro').html(response);
-                
+
             }
         });
     };
 
     cboRubros();
 
-    function cboSubrubros(rubro){
+    function cboSubrubros(rubro) {
         var ru = rubro;
         $.ajax({
             type: "POST",
             url: "./ajax/cboSubrubros.php",
-            data: {ru:ru},
+            data: { ru: ru },
             success: function (response) {
                 $('#subrubro').html(response);
             }
@@ -65,39 +111,39 @@ $(document).ready(function () {
 
     };
 
-    $('#rubro').change(function (e) { 
+    $('#rubro').change(function (e) {
         e.preventDefault();
         var rubro = $('#rubro').val();
         cboSubrubros(rubro);
-        
+
     });
 
-    function cboProveedores(){
+    function cboProveedores() {
         $.ajax({
             type: "POST",
             url: "./ajax/cboProveedores.php",
             success: function (response) {
                 $('#proveedor').html(response);
-                
+
             }
         });
     };
 
     cboProveedores();
 
-    $("#Grubros").click(function (e) { 
+    $("#Grubros").click(function (e) {
         $("#rubros").show();
-        if($("#Grubros").is(':checked')) {  
-            alert("Está activado");  
-        } else {  
-            alert("No está activado");  
-        }  
+        // if ($("#Grubros").is(':checked')) {
+        //     alert("Está activado");
+        // } else {
+        //     alert("No está activado");
+        // }
 
-        
+
     });
-    $("#GGenerales").click(function (e) { 
+    $("#GGenerales").click(function (e) {
         $("#rubros").hide();
-        
+
     });
 
 });
